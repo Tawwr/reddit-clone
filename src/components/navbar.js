147c624b74bd23fcc64c35e22c71fa1ad85689af
Islam Sayed <islam.sayed8@gmail.com>
2022-05-29 22:39:02 +0200

    add Link to logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,49 @@
-import PostForm from "./postForm";
-import { AppBar, Toolbar, Avatar, Box, TextField, IconButton } from "@mui/material";
-import userImag from "../img/97.jpg";
-import redditLogo from "../img/Reddit-Logo.png";
+import PostForm from './postForm';
+import {
+	AppBar,
+	Toolbar,
+	Avatar,
+	Box,
+	TextField,
+	IconButton,
+} from '@mui/material';
+import userImag from '../img/97.jpg';
+import redditLogo from '../img/Reddit-Logo.png';
+import { Link } from 'react-router-dom';
 const NavBar = () => {
 	const handleOpenUserMenu = () => {
 		return;
 	};
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<AppBar color="transparent" position="static" sx={{ boxShadow: "none", background: "#fff" }}>
-				<Toolbar sx={{ justifyContent: "space-between", alignItems: "center", paddingLeft: "48px", paddingRight: "48px" }}>
-					<img src={redditLogo} alt="reddit-logo" width="100" />
-					<Box sx={{ width: "400px" }}>
-						<TextField id="outlined-basic" fullWidth placeholder="Search" variant="outlined" size="small" />
+			<AppBar
+				color='transparent'
+				position='static'
+				sx={{ boxShadow: 'none', background: '#fff' }}
+			>
+				<Toolbar
+					sx={{
+						justifyContent: 'space-between',
+						alignItems: 'center',
+						paddingLeft: '48px',
+						paddingRight: '48px',
+					}}
+				>
+					<Link to='/'>
+						<img src={redditLogo} alt='reddit-logo' width='100' />
+					</Link>
+					<Box sx={{ width: '400px' }}>
+						<TextField
+							id='outlined-basic'
+							fullWidth
+							placeholder='Search'
+							variant='outlined'
+							size='small'
+						/>
 					</Box>
-					<Box sx={{ display: "flex" }}>
+					<Box sx={{ display: 'flex' }}>
 						<IconButton onClick={handleOpenUserMenu}>
-							<Avatar alt="Remy Sharp" src={userImag} />
+							<Avatar alt='Remy Sharp' src={userImag} />
 						</IconButton>
 						<PostForm />
 					</Box>
